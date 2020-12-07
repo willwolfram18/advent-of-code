ade24be6cf7ce92c36822c60c831a4cac5d2f9b5
William Wolfram <willwolfram18@gmail.com>
2020-12-06 21:20:57 -0800

    Day 6 part 2 now

--- 2020/day-6/main.py
+++ 2020/day-6/main.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Callable, List, Dict
 
 def readGroupAnswersFromFile(fileName: str) -> List[List[str]]:
     allGroups: List[List[str]] = []
@@ -19,17 +19,37 @@ def readGroupAnswersFromFile(fileName: str) -> List[List[str]]:
 
     return allGroups
 
-def countUniqueAnswers(groupAnswers: List[str]) -> int:
+def countUniqueAnswers(groupAnswers: List[str], countingStrat: Callable[[List[str], Dict[str,int]], int]) -> int:
     counts: Dict[str, int] = {}
 
     for person in groupAnswers:
         for answer in person:
-            counts[answer] = 1
+            if answer not in counts:
+                counts[answer] = 0
+            counts[answer] += 1
 
-    return len(counts.keys())
+    return countingStrat(groupAnswers, counts)
+
+'''
+Counts the number of answers that had a yes within a group (Part 1)
+'''
+def countAnyAnswer(groupAnswers: List[str], answerCounts: Dict[str,int]) -> int:
+    return len(answerCounts.keys())
+
+'''
+Counts the number of answers that everyone in a group answered yes (Part 2)
+'''
+def countAnswersThatAllAnswered(groupAnswers: List[str], answerCounts: Dict[str, int]) -> int:
+    numUniformAnswers = 0
+
+    for answerAndCount in answerCounts.items():
+        if answerAndCount[1] == len(groupAnswers):
+            numUniformAnswers += 1
+
+    return numUniformAnswers
 
 fileName = input("Enter file to read from: ")
 groups = readGroupAnswersFromFile(fileName)
-totalCounts = sum(map(countUniqueAnswers, groups))
+totalCounts = sum(map(lambda x: countUniqueAnswers(x, countAnswersThatAllAnswered), groups))
 
 print("Total counts:", totalCounts)
